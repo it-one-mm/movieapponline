81cc7f7058394dea8a36bbe651fd5c81786bf370
it-one-mm <itonemm@gmail.com>
2020-01-10 21:13:01 +0630

    first commit

---
+++ app/src/main/java/com/itonemm/movieapponline/CategoryModel.java
@@ -0,0 +1,20 @@
+package com.itonemm.movieapponline;
+
+public class CategoryModel {
+    String categoryjName;
+
+    public CategoryModel(String categoryjName) {
+        this.categoryjName = categoryjName;
+    }
+
+    public CategoryModel() {
+    }
+
+    public String getCategoryjName() {
+        return categoryjName;
+    }
+
+    public void setCategoryjName(String categoryjName) {
+        this.categoryjName = categoryjName;
+    }
+}
